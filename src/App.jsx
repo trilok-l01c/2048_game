7ce015725b0262fdf4553175af02596e238d6d5e
Trilok Lowanshi <trilokl01c@gmail.com>
2026-02-22 21:52:08 +0530

    Added handling key logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const N = 4;
@@ -19,8 +19,11 @@ const addTile = (board) => {
     const tile = emptyTiles[generateRandomNumber(emptyTiles.length)];
 
     board[tile[0]][tile[1]] = Math.random() >= 0.9 ? 4 : 2;
+    return board;
 };
 
+// shifting logic
+const onLeft = (board) => {};
 export default function App() {
     const [board, setBoard] = useState(() => {
         let b = generateBoard();
@@ -28,6 +31,28 @@ export default function App() {
         addTile(b);
         return b;
     });
+    // ******** Working area *************************
+    // using useEffect
+    useEffect(() => {}, [board]);
+
+    // handle key down
+    const handleKeydown = (e) => {
+        addTile(board);
+        const key = e.key;
+        if (key === "ArrowUp" || key === "w") {
+            // handle logic for up
+        } else if (key === "ArrowDown" || key === "s") {
+            // handle logic for down
+        } else if (key === "ArrowLeft" || key === "a") {
+            // handle logic for left
+        } else if (key === "ArrowRight" || key === "d") {
+            // handle logic for right
+        } else return;
+
+        // handle remaining tasks
+    };
+    // ***********************************************
+    window.addEventListener("keydown", handleKeydown);
     return (
         <div className="container">
             <h1>2048 Board game</h1>
